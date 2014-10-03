3470e7f5d4a96a609ace71f901e0ff9086077f22
GreenFOX Media Patric Schönenberger <info@green-fox-media.com>
2014-10-03 06:52:46 +0200

    Content Initialisierung und Login Screen mit login feldern hinzugefügt

--- GreenFOXMedia/gen/com/greenfoxmedia/greenfoxmedia/R.java
+++ GreenFOXMedia/gen/com/greenfoxmedia/greenfoxmedia/R.java
@@ -1091,7 +1091,7 @@ containing a value of this type.
         public static final int action_menu_divider=0x7f050016;
         public static final int action_menu_presenter=0x7f050017;
         public static final int action_mode_close_button=0x7f050024;
-        public static final int action_settings=0x7f05003e;
+        public static final int action_settings=0x7f050040;
         public static final int activity_chooser_view_content=0x7f050025;
         public static final int always=0x7f05000b;
         public static final int beginning=0x7f050011;
@@ -1116,6 +1116,8 @@ containing a value of this type.
         public static final int never=0x7f050009;
         public static final int none=0x7f050010;
         public static final int normal=0x7f050000;
+        public static final int passwordField=0x7f05003d;
+        public static final int progressBar1=0x7f05003e;
         public static final int progress_circular=0x7f050018;
         public static final int progress_horizontal=0x7f050019;
         public static final int radio=0x7f05002f;
@@ -1136,12 +1138,12 @@ containing a value of this type.
         public static final int split_action_bar=0x7f05001e;
         public static final int submit_area=0x7f050039;
         public static final int tabMode=0x7f050002;
-        public static final int textView1=0x7f05003d;
-        public static final int textView2=0x7f05003c;
+        public static final int textView1=0x7f05003f;
         public static final int title=0x7f05002b;
         public static final int top_action_bar=0x7f050020;
         public static final int up=0x7f050021;
         public static final int useLogo=0x7f050003;
+        public static final int usernameField=0x7f05003c;
         public static final int withText=0x7f05000c;
     }
     public static final class integer {
@@ -1194,11 +1196,13 @@ containing a value of this type.
         public static final int abc_search_dropdown_item_icons_2line=0x7f030015;
         public static final int abc_search_view=0x7f030016;
         public static final int abc_simple_decor=0x7f030017;
-        public static final int activity_start_up_screen=0x7f030018;
-        public static final int support_simple_spinner_dropdown_item=0x7f030019;
+        public static final int activity_login_screen=0x7f030018;
+        public static final int activity_start_up_screen=0x7f030019;
+        public static final int support_simple_spinner_dropdown_item=0x7f03001a;
     }
     public static final class menu {
-        public static final int start_up_screen=0x7f0c0000;
+        public static final int login_screen=0x7f0c0000;
+        public static final int start_up_screen=0x7f0c0001;
     }
     public static final class string {
         /**  Content description for the action bar "home" affordance. [CHAR LIMIT=NONE] 
@@ -1244,6 +1248,9 @@ containing a value of this type.
         public static final int action_settings=0x7f0a000f;
         public static final int app_name=0x7f0a000d;
         public static final int hello_world=0x7f0a000e;
+        public static final int password=0x7f0a0012;
+        public static final int title_activity_login_screen=0x7f0a0010;
+        public static final int userID=0x7f0a0011;
     }
     public static final class style {
         /** 
--- GreenFOXMedia/src/com/greenfoxmedia/greenfoxmedia/StartUpScreenActivity.java
+++ GreenFOXMedia/src/com/greenfoxmedia/greenfoxmedia/StartUpScreenActivity.java
@@ -1,36 +1,86 @@
 package com.greenfoxmedia.greenfoxmedia;
 
-import android.support.v7.app.ActionBarActivity;
+import java.util.Random;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
-
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 public class StartUpScreenActivity extends ActionBarActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_start_up_screen);
-    }
+	public ProgressBar m_bar;
 
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_start_up_screen);
+		
+		final String[] files = new String[100];
+		files[0] = "Lade Benutzerdaten...";
+		files[1] = "Kontaktiere Datenbank...";
+		files[2] = "Syncronisiere Inhalte...";
+		files[3] = "Initialisiere Schnittstellen...";
+		files[4] = "generiere Views...";
+		files[5] = "Lade Bilder...";
+		files[7] = "";
+		files[8] = "";
+		files[9] = "";
+		files[10] = "";
+		files[11] = "";
+		
+		CountDownTimer timer = new CountDownTimer(3000, 100) {
+			Random r = new Random();
+			
+			@Override
+			public void onTick(long millisUntilFinished) {
+				int i1 = r.nextInt(12 - 0);
+				TextView showText = (TextView) findViewById(R.id.textView1);
+				showText.setText(files[i1]);
+			}
+			
+			@Override
+			public void onFinish() {
+				CountDownTimer startActivity = new CountDownTimer(1500, 100) {
+					@Override
+					public void onTick(long millisUntilFinished) {
+						TextView showText = (TextView) findViewById(R.id.textView1);
+						showText.setText("Starte App...");
+					}
+					
+					@Override
+					public void onFinish() {
+						startActivity(new Intent(getApplicationContext(), LoginScreenActivity.class));
+					}
+				};
+				startActivity.start();
+			}
+		};
+		
+		timer.start();
+		
+	}
+	
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.start_up_screen, menu);
+		return true;
+	}
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.start_up_screen, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
 }
